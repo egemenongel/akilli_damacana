f5d55ba5f759ae96b2f02e8a09196477c79bfd39
Egemen Öngel <egemenongel@hotmail.com>
2022-01-04 00:28:10 +0300

    Update custom form field

--- lib/core/components/bordered_form_field.dart
+++ lib/core/components/bordered_form_field.dart
@@ -3,21 +3,23 @@ import 'package:akilli_damacana/core/extension/context_extension.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class BorderedFormField extends StatelessWidget {
-  const BorderedFormField({
-    Key? key,
-    required this.keyboardType,
-    required this.labelText,
-    required this.assetPath,
-    required this.textInputAction,
-    this.textEditingController,
-    this.isObscured,
-  }) : super(key: key);
+  const BorderedFormField(
+      {Key? key,
+      required this.keyboardType,
+      required this.labelText,
+      required this.assetPath,
+      required this.textInputAction,
+      this.textEditingController,
+      this.isObscured,
+      this.validator})
+      : super(key: key);
   final TextInputType keyboardType;
   final String labelText;
   final String assetPath;
   final TextInputAction textInputAction;
   final TextEditingController? textEditingController;
   final bool? isObscured;
+  final String? Function(String?)? validator;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -50,6 +52,7 @@ class BorderedFormField extends StatelessWidget {
         ),
       ),
       controller: textEditingController,
+      validator: validator,
       textInputAction: textInputAction,
       keyboardType: keyboardType,
       style: context.textTheme.headline6!.copyWith(
