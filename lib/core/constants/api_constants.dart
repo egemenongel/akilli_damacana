7f925bd017e513e6d5f19fb191938c2c13da03e4
Egemen Öngel <egemenongel@hotmail.com>
2022-01-03 14:48:52 +0300

    Create api constants

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  static const baseUrl = "https://task.bixos.io/api/v1/";
+  static const login = "${baseUrl}auth/login";
+  static const productsList = "${baseUrl}products";
+  static const orderCreate = "${baseUrl}order";
+  static const profile = "${baseUrl}profile";
+  static const profilePictureEdit = "${baseUrl}profile/picture";
+}
