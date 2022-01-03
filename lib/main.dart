2118387d0dda81919ffba38a62127c5d651c6e51
Egemen Öngel <egemenongel@hotmail.com>
2022-01-03 14:52:21 +0300

    Update main with ensureInitialized method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:akilli_damacana/core/components/custom_bottom_nav_bar.dart';
 import 'package:akilli_damacana/core/theme/app_theme.dart';
-import 'package:akilli_damacana/features/_product/widgets/custom_bottom_nav_bar.dart';
-import 'package:akilli_damacana/features/auth/login/login_view.dart';
+import 'package:akilli_damacana/features/auth/login/view/login_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
